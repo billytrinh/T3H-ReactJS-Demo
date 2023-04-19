2e3dd05cf5aebb2decb234eaae4e6450b9fc2fd9
macbook <quanghoa.trinh@gmail.com>
2023-04-19 20:11:09 +0700

    up code route - bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/Header';
-import Classroom from './components/Classroom';
-
-function App() {
-  return (
-    <div className='row'>
-      <div className='col-3'></div>
-      <div className='col-6'>
-      <h1>DEMO ReactJS</h1>
-      <Header/>
-      <Classroom/>
-    </div>
-    </div>
-  );
+import React from "react";
+import Header from "./components/Header";
+import HomePage from "./pages/HomePage"
+import SearchPage from "./pages/SearchPage"
+import ProductPage from "./pages/ProductPage"
+import { Route, Routes } from "react-router-dom";
+class App extends React.Component{
+    render(){
+      return (
+        <div>
+          <Header />
+          <section>
+            <div className="container">
+              <Routes>
+                  <Route path="/" element={<HomePage />} />
+                  <Route path="/search" element={<SearchPage />} />
+                  <Route path="/product" element={<ProductPage />} />
+              </Routes>
+            </div>
+          </section>
+        </div>
+      );
+    }
 }
-
 export default App;
---
+++ src/pages/ProductPage.js
@@ -0,0 +1,6 @@
+import React from "react";
+export default class ProductPage extends React.Component{
+    render(){
+        return (<h1>Product Detail Page</h1>);
+    }
+}
